4a1b0b7727400af155afe897ba657a34b795bda1
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-09-29 19:39:26 -0300

    GoLang Update

--- BS.go
+++ BS.go
@@ -1,6 +1,24 @@
 // Go
-package BS
+package main
+
+import "fmt"
+
+func bubbleSort(listage []int) []int {
+	length := len(listage)
+
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-i-1; j++ {
+			if listage[j] > listage[j+1] {
+				listage[j], listage[j+1] = listage[j+1], listage[j]
+			}
+		}
+	}
+	return listage
+}
 
 func main() {
+	listage := []int{4, 8, 87, 54, 12, 3, 65, 99, 30, 43}
 
+	newListage := bubbleSort(listage)
+	fmt.Println(newListage)
 }
